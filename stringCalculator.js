c1508aa5520d2284b6365103ba4a193708b7d05f
Anukrit Tiwari <anukrit01@gmail.com>
2024-12-19 20:47:47 +0530

    updated add function to handle negative numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -10,6 +10,13 @@ function add(numbers) {
     }
 
     const nums = numbers.split(delimiter).map(Number);
+    
+    //to handle negative numbers
+    const negatives = nums.filter(num => num < 0);
+    if (negatives.length > 0) {
+        throw new Error(`negatives not allowed: ${negatives.join(', ')}`);
+    }
+
     return nums.reduce((sum, num) => sum + num, 0);
 }
 
