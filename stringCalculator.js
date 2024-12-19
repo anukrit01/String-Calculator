ffe93ca105f75602c29ea1035bc3ea49bcb92a21
Anukrit Tiwari <anukrit01@gmail.com>
2024-12-19 20:10:37 +0530

    updated add function to handle two numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,9 @@
 function add(numbers) {
     if (numbers === "") return 0;
-    return parseInt(numbers);
+
+    //to add two numbers
+    const nums = numbers.split(',').map(Number);
+    return nums.reduce((sum, num) => sum + num, 0);
 }
 
 module.exports = add;
