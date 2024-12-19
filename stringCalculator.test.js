fed44081141966aa18acc717de75aefc4c67251f
Anukrit Tiwari <anukrit01@gmail.com>
2024-12-19 20:37:16 +0530

    test for negative numbers added

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -23,3 +23,7 @@ test('handles new lines as delimiters', () => {
 test('supports different delimiters', () => {
     expect(add("//;\n1;2")).toBe(3);
 });
+
+test('throws an exception for negative numbers', () => {
+    expect(() => add("1,-2,3,-4")).toThrow("negatives not allowed: -2, -4");
+});
