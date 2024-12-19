b1141e163e67bd089f5b016ac6a7ffb5e7fb2b0d
Anukrit Tiwari <anukrit01@gmail.com>
2024-12-19 20:29:25 +0530

    added test for different delimiters

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -19,3 +19,7 @@ test('returns the sum of an unknown amount of numbers', () => {
 test('handles new lines as delimiters', () => {
     expect(add("1\n2,3")).toBe(6);
 });
+
+test('supports different delimiters', () => {
+    expect(add("//;\n1;2")).toBe(3);
+});
